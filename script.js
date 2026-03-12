1981eb19932650bde14b5eee93535502060caa00
Samiuzzaman Chowdhury <chowdhurysamiuzzaman@gmail.com>
2026-03-12 23:59:51 +0600

    added completed buttton functionality

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
+// Date System Start
+const dateSection = document.getElementById("date-section");
+const p = document.createElement("p");
+p.innerHTML = `
+  <span>${currentDate().week}, </span> <br> <span class="text-md font-medium">${currentDate().todayDate}</span></p>
+`;
+dateSection.appendChild(p);
+// Date System End
+
 // Set Random Background Color
 document
   .getElementById("random-color-btn")
   .addEventListener("click", function () {
     document.getElementById("body").style.backgroundColor = randomColor();
   });
+
+// Complete Button Task -1
+document
+  .getElementById("complete-btn-one")
+  .addEventListener("click", function () {
+    completeButtonTask("complete-btn-one", "complete-heading-one");
+  });
+
+// Complete Button Task -2
+document
+  .getElementById("complete-btn-two")
+  .addEventListener("click", function () {
+    completeButtonTask("complete-btn-two", "complete-heading-two");
+  });
+
+// Complete Button Task - 3
+document
+  .getElementById("complete-btn-three")
+  .addEventListener("click", function () {
+    completeButtonTask("complete-btn-three", "complete-heading-three");
+  });
+
+// Complete Button Task -4
+document
+  .getElementById("complete-btn-four")
+  .addEventListener("click", function () {
+    completeButtonTask("complete-btn-four", "complete-heading-four");
+  });
+
+// Complete Button Task - 5
+document
+  .getElementById("complete-btn-five")
+  .addEventListener("click", function () {
+    completeButtonTask("complete-btn-five", "complete-heading-five");
+  });
+
+// Complete Button Task -6
+document
+  .getElementById("complete-btn-six")
+  .addEventListener("click", function () {
+    completeButtonTask("complete-btn-six", "complete-heading-six");
+  });
+
+
--- utils.js
+++ utils.js
@@ -3,5 +3,43 @@ function randomColor() {
   const g = Math.round(Math.random() * 255);
   const b = Math.round(Math.random() * 255);
   const rgb = `rgb(${r},${g},${b})`;
-  return rgb  
+  return rgb;
+}
+
+function completeButtonTask(btnId, heading) {
+  const headingText = document.getElementById(heading).innerText;
+  console.log("Heading: ", headingText);
+  alert("Board Updated Successfully");
+  const completeTask = document.getElementById("complete-task");
+  completeTask.innerText = parseInt(completeTask.innerText) + 1;
+  const pendingTask = document.getElementById("pending-task");
+  pendingTask.innerText = parseInt(pendingTask.innerText) - 1;
+
+  const el = document.getElementById(btnId);
+  el.setAttribute("disabled", true);
+  el.style.backgroundColor = "rgba(55, 81, 253, 0.22)";
+  el.style.color = "#FFFFFF";
+
+  // Add message aside section
+  const messageContainer = document.getElementById("message-container");
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <p class="text-left bg-[#f4f7ff] [#00303C] p-3 rounded-lg text-sm">You have Complete The Task <span class="font-medium">${headingText}</span> at ${currentTime()}</p>
+  `;
+  messageContainer.appendChild(div);
+  if (pendingTask.innerText <= 0) {
+    alert("congratulations!!! You have completed all the current tasks");
+  }
+}
+
+function currentDate() {
+  const date = new Date();
+  const week = date.toString().split(" ")[0];
+  const todayDate = date.toString().split(" ").slice(1, 4).join(" ");
+  return { week, todayDate };
+}
+
+function currentTime() {
+  const time = new Date().toLocaleTimeString("en-BD");
+  return time;
 }
